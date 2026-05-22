e1355598179c032564e0d9a65b17bfa13d6de23b
Nicholas Little <nicholas.little@exactasystems.com>
2026-05-22 09:55:19 -0500

    Added next config into the project this time

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  env: {
+    TABLEAU_HOST: process.env.TABLEAU_HOST,
+    TABLEAU_PAT_NAME: process.env.TABLEAU_PAT_NAME,
+    TABLEAU_PAT_SECRET: process.env.TABLEAU_PAT_SECRET,
+    TABLEAU_SITE_CONTENT_URL: process.env.TABLEAU_SITE_CONTENT_URL,
+    TABLEAU_REPORT_COMPS_VIEW_ID: process.env.TABLEAU_REPORT_COMPS_VIEW_ID,
+    TABLEAU_MACHINE_COUNT_VIEW_ID: process.env.TABLEAU_MACHINE_COUNT_VIEW_ID,
+    TABLEAU_MAP_VIEW_ID: process.env.TABLEAU_MAP_VIEW_ID,
+    EXACTA_API_BASE_URL: process.env.EXACTA_API_BASE_URL,
+    EXACTA_API_KEY: process.env.EXACTA_API_KEY,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
